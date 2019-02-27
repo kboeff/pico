ef6f9ed937e03a7dd67b33b95ca42d4926d2f1da
Kamen Boev <kamen.boev@gmail.com>
2019-02-27 14:08:49 +0000

    added -random- package, not yet implemented

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
+let start = new Date();
+
 const permutate = require('./permutations/permutations');
 const combine = require('./permutations/combinations');
 
 const fs = require('fs');
 
 
-let sample = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', '1', '2', '3'];
-// let sample = ['a', 'b', 'c', 'd', '2', 's'];
+let sample = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', '1', '2', '3', '4'];
+// let sample = ['a', 'b', 'c'];
 let size = 6;
 
 let combinations = combine(sample, size);
-// console.log(combinations);
-// console.log(permutate(sample).length);
 
+/*
+let result = [];
+for (let i in combinations) {
+  // console.log(permutate(combinations[i]).join('\n'));
+  result.push(permutate(combinations[i],size).join('\n'));
+}
+*/
 
 let logger = fs.createWriteStream('./file.txt', {
   flags: 'a' // 'a' means appending 
 });
 
 for (let i in combinations) {
-  // console.log(permutate(combinations[i]).join('\n'));
   logger.write(permutate(combinations[i]).join('\n'));
   logger.write('\n');
 }
 logger.end();
+ 
+console.log('Done in ', start - new Date());
--- permutations/permutations.js
+++ permutations/permutations.js
@@ -1,31 +1,31 @@
 /**
  * @param {*[]} permutationOptions
+ * @param {number} permutationLength
  * @return {*[]}
  */
-module.exports = function permutateWithoutRepetitions(permutationOptions) {
-  if (permutationOptions.length === 1) {
-    return [permutationOptions];
+module.exports = function permutateWithRepetitions(
+  permutationOptions,
+  permutationLength = permutationOptions.length,
+) {
+  if (permutationLength === 1) {
+    return permutationOptions.map(permutationOption => [permutationOption]);
   }
 
   // Init permutations array.
   const permutations = [];
 
-  // Get all permutations for permutationOptions excluding the first element.
-  const smallerPermutations = permutateWithoutRepetitions(permutationOptions.slice(1));
+  // Get smaller permutations.
+  const smallerPermutations = permutateWithRepetitions(
+    permutationOptions,
+    permutationLength - 1,
+  );
 
-  // Insert first option into every possible position of every smaller permutation.
-  const firstOption = permutationOptions[0];
-
-  for (let permIndex = 0; permIndex < smallerPermutations.length; permIndex += 1) {
-    const smallerPermutation = smallerPermutations[permIndex];
-
-    // Insert first option into every possible position of smallerPermutation.
-    for (let positionIndex = 0; positionIndex <= smallerPermutation.length; positionIndex += 1) {
-      const permutationPrefix = smallerPermutation.slice(0, positionIndex);
-      const permutationSuffix = smallerPermutation.slice(positionIndex);
-      permutations.push(permutationPrefix.concat([firstOption], permutationSuffix));
-    }
-  }
+  // Go through all options and join it to the smaller permutations.
+  permutationOptions.forEach((currentOption) => {
+    smallerPermutations.forEach((smallerPermutation) => {
+      permutations.push([currentOption].concat(smallerPermutation).join(''));
+    });
+  });
 
   return permutations;
 }
